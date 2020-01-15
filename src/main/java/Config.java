f7022ca5f1c5dca29bda6164f557f6de7de2dffe
SowjanyaPutrevu <putrevusowjanya@gmail.com>
2020-01-15 08:06:20 +0100

    First commit

---
+++ src/main/java/Config.java
@@ -0,0 +1,7 @@
+public class Config {
+
+    public static String JIRA_USERNAME = "bhargav.sarvepalli@yahoo.com";
+    public static String TESTRAIL_USERNAME = "bhargav.sarvepalli@yahoo.com";
+    public static String JIRA_PASSWORD = "iambond1king";
+    public static String TESTRAIL_PASSWORD = "Testrail123";
+}
---
+++ src/main/java/CreateTestRailFromJira.java
@@ -0,0 +1,35 @@
+import com.github.ppadial.testrail.client.TestRailClient;
+import com.github.ppadial.testrail.client.apiClient.ApiClient;
+import com.github.ppadial.testrail.client.model.TRProject;
+import com.github.ppadial.testrail.client.model.TRSection;
+import net.rcarz.jiraclient.BasicCredentials;
+import net.rcarz.jiraclient.ICredentials;
+import net.rcarz.jiraclient.Issue;
+import net.rcarz.jiraclient.JiraClient;
+
+import java.util.List;
+
+public class CreateTestRailFromJira {
+
+    public static void main(String[] args) throws Exception {
+
+        ApiClient apiClient = new ApiClient("https://bsarvepallitest.testrail.io", Config.TESTRAIL_USERNAME, "Testrail123");
+        TestRailClient testRailClient = new TestRailClient(apiClient);
+        TRProject trProject = testRailClient.projectApi().getProject(1);
+
+        JiraClient jiraClient = new JiraClient("http://bsarvepalli-test.atlassian.net", new BasicCredentials(Config.JIRA_USERNAME, "iHdkd5xOPa3fqXxE88ud898D"));
+        Issue issue = jiraClient.getIssue("TEST-1");
+        String jira_ID = issue.getId(); //get jira issue id
+        String desc = issue.getSummary() ; //get issue title
+        String body = issue.getDescription(); //get jira issue body;
+        TRSection section = addSection(testRailClient, issue.getUrl(), jira_ID + ": " + desc);
+
+
+        System.out.println(body);
+
+    }
+
+    public static TRSection addSection(TestRailClient testRailClient, String desc, String name) throws Exception {
+       return testRailClient.sectionApi().addSection(1, desc, null, null, name);
+    }
+}
