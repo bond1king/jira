03435a72261e9f01226a19a47aa0e91694ff50f1
SowjanyaPutrevu <putrevusowjanya@gmail.com>
2020-01-15 08:26:28 +0100

    Second commit

--- src/main/java/CreateTestRailFromJira.java
+++ src/main/java/CreateTestRailFromJira.java
@@ -1,3 +1,5 @@
+import com.codepine.api.testrail.TestRail;
+import com.codepine.api.testrail.model.Section;
 import com.github.ppadial.testrail.client.TestRailClient;
 import com.github.ppadial.testrail.client.apiClient.ApiClient;
 import com.github.ppadial.testrail.client.model.TRProject;
@@ -13,17 +15,23 @@ public class CreateTestRailFromJira {
 
     public static void main(String[] args) throws Exception {
 
-        ApiClient apiClient = new ApiClient("https://bsarvepallitest.testrail.io", Config.TESTRAIL_USERNAME, "Testrail123");
+        /*ApiClient apiClient = new ApiClient("https://bsarvepallitest.testrail.io", Config.TESTRAIL_USERNAME, "Testrail123");
         TestRailClient testRailClient = new TestRailClient(apiClient);
-        TRProject trProject = testRailClient.projectApi().getProject(1);
+        TRProject trProject = testRailClient.projectApi().getProject(1);*/
 
         JiraClient jiraClient = new JiraClient("http://bsarvepalli-test.atlassian.net", new BasicCredentials(Config.JIRA_USERNAME, "iHdkd5xOPa3fqXxE88ud898D"));
         Issue issue = jiraClient.getIssue("TEST-1");
         String jira_ID = issue.getId(); //get jira issue id
         String desc = issue.getSummary() ; //get issue title
         String body = issue.getDescription(); //get jira issue body;
-        TRSection section = addSection(testRailClient, issue.getUrl(), jira_ID + ": " + desc);
 
+        TestRail testRail = TestRail.builder("https://bsarvepallitest.testrail.io", Config.TESTRAIL_USERNAME, "Testrail123").applicationName("playground").build();
+        Section section = new Section();
+        section.setDescription(issue.getUrl());
+        section.setName(jira_ID + ": " + desc);
+        testRail.sections().add(1, section).execute();
+
+        /*TRSection section = addSection(testRailClient, issue.getUrl(), jira_ID + ": " + desc);*/
 
         System.out.println(body);
 
